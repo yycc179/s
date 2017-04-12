3b4ae4c89518e01a6e890dc2010206876c3cca97
Yang Yang <yangyang@CDDEV01.cd.ad.availink.com>
2017-04-12 11:57:41 +0800

    fix status valid time all bug

--- routes/server/utils.js
+++ routes/server/utils.js
@@ -1,9 +1,9 @@
 exports.getUpdatedAt = function(time) {
     var expire = new Date();
-    expire.setSeconds(0);
-    expire.setMilliseconds(0);
     var res = {};
     if (time > 0) {
+        expire.setSeconds(0);
+        expire.setMilliseconds(0);
         expire.setMinutes(expire.getMinutes() - time);
         res.$gt = expire;
     }
